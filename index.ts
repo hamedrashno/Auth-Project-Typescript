bc93103d0a518aabe0c082c89ae670c5ebbe5e7e
h.rashno <h.rashno@novatechco.net>
2026-09-02 16:54:34 +0330

    Test

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import expressConfig from "./src/startup/config";
 import dbConfig from "./src/startup/db";
+import express, { Express, Request, Response, application } from "express";
+
 dotenv.config();
-expressConfig()
+expressConfig();
 dbConfig();
